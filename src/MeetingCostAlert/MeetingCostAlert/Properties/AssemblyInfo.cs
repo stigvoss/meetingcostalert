70e1e2ab88898fb41516a93af606c9e7cb622aa4
Stig Voss <951318821eb9a415aaf3175feb82ceca@50ce4c50.ee>
2021-03-16 21:18:42 +0100

    Initial code commit.

---
+++ src/MeetingCostAlert/MeetingCostAlert/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Meeting Cost Alert")]
+[assembly: AssemblyDescription("Displays the cost of a meeting to the organizer when sending a meeting request.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Meeting Cost Alert")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fd925e14-f52d-4d29-a6fd-7f5bb93c3c7c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
---
+++ src/MeetingCostAlert/MeetingCostAlert/ThisAddIn.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+using MeetingCostAlert.Properties;
+using Microsoft.Office.Interop.Outlook;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace MeetingCostAlert
+{
+    public partial class ThisAddIn
+    {
+        private int hourlyRate;
+        private int trivialCostThreshold;
+        private string locale;
+
+        private void ThisAddIn_Startup(object sender, EventArgs e)
+        {
+            Settings.Default.Upgrade();
+
+            this.hourlyRate = Settings.Default.HourlyRate;
+            this.trivialCostThreshold = Settings.Default.TrivialCostThreshold;
+            this.locale = Settings.Default.Locale;
+
+            Application.ItemSend += Application_ItemSend;
+        }
+
+        private void Application_ItemSend(object Item, ref bool Cancel)
+        {
+            if (Item is MeetingItem meeting)
+            {
+                AppointmentItem appointment = meeting.GetAssociatedAppointment(false);
+                RecurrencePattern recurrancePattern = appointment.GetRecurrencePattern();
+
+                if (!IsOrganizedByCurrentUser(appointment))
+                {
+                    return;
+                }
+
+                int attendees = appointment.Recipients.Count;
+                TimeSpan duration = TimeSpan.FromMinutes(appointment.Duration);
+
+                double cost = duration.TotalHours * attendees * hourlyRate;
+                double timeCost = duration.TotalHours * attendees;
+
+                if (cost <= this.trivialCostThreshold)
+                {
+                    return;
+                }
+
+                string message = appointment.IsRecurring
+                    ? string.Format(
+                        CultureInfo.CreateSpecificCulture(this.locale),
+                        "The cost of this recurring meeting is an estimated {1:C0} per occurrence and {3:C0} in total.\n" +
+                        "Do you wish to continue?",
+                        timeCost,
+                        cost,
+                        timeCost * recurrancePattern.Occurrences,
+                        cost * recurrancePattern.Occurrences)
+                    : string.Format(
+                        CultureInfo.CreateSpecificCulture(this.locale),
+                        "The cost of this meeting is an estimated {1:C0}\n" +
+                        "Do you wish to continue?",
+                        timeCost,
+                        cost);
+
+                DialogResult result = MessageBox.Show(
+                    message,
+                    "Meeting Cost",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+
+                Cancel = result != DialogResult.Yes;
+            }
+        }
+
+        private bool IsOrganizedByCurrentUser(AppointmentItem appointment)
+        {
+            if (appointment == null)
+            {
+                return false;
+            }
+
+            AddressEntry currentUser = Application.Session.CurrentUser.AddressEntry;
+            return currentUser.ID == appointment.GetOrganizer().ID;
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            // Note: Outlook no longer raises this event. If you have code that 
+            //    must run when Outlook shuts down, see https://go.microsoft.com/fwlink/?LinkId=506785
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+
+        #endregion
+    }
+}
